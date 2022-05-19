a2c306f71bb11c65588f50b8b7ab0e7a1c93d5fe
Bryant Brock <47164007+bryantbrock@users.noreply.github.com>
2022-05-19 17:02:43 -0500

    Create omit.ts

---
+++ omit.ts
@@ -0,0 +1,52 @@
+const getIndex = (value: string, subtractions: number[]) => {
+  const num = Number(value);
+
+  return !Number.isNaN(num)
+    ? subtractions.reduce(
+        // eslint-disable-next-line no-param-reassign
+        (index, sub) => (sub < index ? index - 1 : index),
+        num
+      )
+    : undefined;
+};
+
+export function omit(obj: object | [] | undefined, paths: string[] | undefined = []) {
+  if (!obj || !paths) {
+    return undefined;
+  }
+
+  const subtractions: number[] = [];
+
+  return paths.reduce((acc, path) => {
+    const deepIndex = path.indexOf('.');
+    const isArray = Array.isArray(acc);
+
+    if (deepIndex !== -1) {
+      const rawKey = path.slice(0, deepIndex);
+      const key = (isArray ? getIndex(rawKey, subtractions) : rawKey) as keyof typeof acc;
+      const nestedKeys = path.slice(deepIndex + 1);
+
+      if (key) {
+        acc[key] = omit(acc[key], [nestedKeys]) as never;
+      }
+    } else {
+      if (Array.isArray(acc)) {
+        const idx = getIndex(path, subtractions);
+
+        if (idx === undefined) {
+          return acc;
+        }
+
+        // eslint-disable-next-line no-param-reassign
+        acc = acc.slice(0, idx).concat(acc.slice(idx + 1));
+        subtractions.push(idx);
+      } else {
+        if (path in acc) {
+          delete acc[path as keyof typeof acc];
+        }
+      }
+    }
+
+    return acc;
+  }, obj);
+}
